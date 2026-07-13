a304724a4da141a1b5be75e2967689ed4a0dfb1c
Theophilus Jacob <theophilusjacob@gmail.com>
2026-07-14 00:19:12 +0100

    Functions

---
+++ cs50/code1.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+
+first, last = name.split()
+
+print(f"Hello, {first}")
--- cs50/hello.py
+++ cs50/hello.py
@@ -1,9 +1,20 @@
 #print("Hello, World!")
 
-NAME = input("What's your name? ")
+name = input("What's your name? ").strip().title()
+
+#removing space from the right and left from str
+name = name.strip()
 
 #print("Hello, " + NAME)
 
 #print("Hello,", NAME)
 
-print(f"Hello, {NAME}")
+# Capitalize the first word in a string
+#name = name.capitalize()
+
+#capitalize the first character of each word in a string 
+#name = name.title()
+
+#name = name.strip().title()
+
+print(f"Hello, {name}")
