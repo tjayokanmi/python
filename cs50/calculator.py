aa8f495d68b7fbeb76b5ee4475695080f65b88b0
Theophilus Jacob <theophilusjacob@gmail.com>
2026-07-14 01:54:19 +0100

    functions and scope

--- cs50/calculator.py
+++ cs50/calculator.py
@@ -1,15 +1,24 @@
-# x = input("What's x " )
+# # x = input("What's x " )
 
-# y = input("What's y  ?" )
+# # y = input("What's y  ?" )
 
-# z = int(x) + int(y)
+# # z = int(x) + int(y)
 
-x = float(input("What's X? "))
-y = float(input("What's Y? "))
+# x = float(input("What's X? "))
+# y = float(input("What's Y? "))
 
-# z = round(x + y)
-z = x/y
+# # z = round(x + y)
+# z = x/y
 
-# print(f"{z:,}")
+# # print(f"{z:,}")
 
-print(f"{z:.4f}")
+# print(f"{z:.4f}")
+
+def main():
+    x = int(input("what's X? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n
+
+main()
