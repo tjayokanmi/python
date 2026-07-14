c98009b9033c42457973b90b44c7b6a6b394ed7b
Theophilus Jacob <theophilusjacob@gmail.com>
2026-07-14 01:34:33 +0100

    floats

--- cs50/calculator.py
+++ cs50/calculator.py
@@ -1,7 +1,15 @@
-x = input("What's x " )
+# x = input("What's x " )
 
-y = input("What's y ?" )
+# y = input("What's y  ?" )
 
-z = int(x) + int(y)
+# z = int(x) + int(y)
 
-print(z)
+x = float(input("What's X? "))
+y = float(input("What's Y? "))
+
+# z = round(x + y)
+z = x/y
+
+# print(f"{z:,}")
+
+print(f"{z:.4f}")
